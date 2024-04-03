89c0f68867e8aa22febd47d80ab40b199640f300
futing <447072785@qq.com>
2024-04-03 18:21:53 +0800

    写到了一些的两数之和，未调试成功

--- javalib/src/main/java/com/futing/javalib/algorithm/Algorithm.java
+++ javalib/src/main/java/com/futing/javalib/algorithm/Algorithm.java
@@ -1,8 +1,86 @@
 package com.futing.javalib.algorithm;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 import java.util.HashMap;
+import java.util.LinkedList;
+
+import jdk.internal.net.http.common.Log;
 
 public class Algorithm {
+
+    public static void main(String[] args) {
+        //
+    }
+
+    //     Definition for singly-linked list.
+    public class ListNode {
+        int      val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //2.两数相加
+    //https://leetcode.cn/problems/add-two-numbers/discussion/
+
+    /**
+     * 2->4>3
+     * 5>6>4
+     * <p>
+     * 7->0>8
+     * <p>
+     * 342+465 =807
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultNode = new ListNode();
+        int inc = 0;
+        String R = "";
+        while (l1 != null || l2 != null) {
+
+            int val1 = 0;
+            if (l1 != null) {
+                val1 = l1.val;
+                R+=val1;
+                l1 = l1.next;
+            }
+            int val2 = 0;
+            if (l2 != null) {
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+
+            int addResult = val1 + val2 + inc;
+            inc = 0;
+            if (addResult >= 10) {
+                addResult = addResult - 10;
+                inc = 1;
+            }
+            if (resultNode.next != null) {
+                resultNode = resultNode.next;
+            }
+            resultNode.val = Integer.parseInt(R);
+            resultNode.next = new ListNode();
+        }
+
+        resultNode.next = null;
+        return resultNode;
+    }
+
     //1、两数之和 [2,7,11,15]
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> findHaspMap = new HashMap<>();
@@ -62,7 +140,6 @@ public class Algorithm {
 
         return Math.max(f1, f2);
     }
-
 
 
     //
